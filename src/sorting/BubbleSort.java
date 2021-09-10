892e021011d2c2f83847bc823dee3b301f2ebc2f
Shalini-CEG <ahana298@gmail.com>
2021-09-10 15:29:16 +0530

    Sorting

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,28 @@
+package sorting;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int arr[] = {3, 8, 5, 7, 4};
+        int n = arr.length;
+        bubbleSort(arr, n);
+
+        for (int i: arr) {
+            System.out.println(i);
+        }
+    }
+
+    private static void bubbleSort(int[] arr, int n) {
+        boolean swapped = false;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if(arr[j+1] < arr[j]){
+                    swapped = true;
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            if(!swapped) break;
+        }
+    }
+}
--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -1,23 +1,10 @@
 package sorting;
 
 public class InsertionSort {
-    static void insert(int arr[],int i)
-    {
-//        int temp = arr[i];
-//        int j = i - 1;
-//
-//        while((j >= 0) && arr[j+1] > temp){
-//            arr[j+1] = arr[j];
-//            j--;
-//        }
-//
-//        arr[j+1] = temp;
-    }
 
     //Function to sort the array using insertion sort algorithm.
     public static void insertionSort(int arr[], int n) {
         for (int i = 1; i < n; i++) {
-//            insert(arr, i);
             int temp = arr[i];
             int j = i - 1;
 
